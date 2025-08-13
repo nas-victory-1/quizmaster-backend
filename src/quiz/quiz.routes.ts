fc09726efe0641adbae260f72b251aa4b3b174e0
Nasser Samiru <nasserjadagwa@gmail.com>
2025-08-13 12:30:22 +0000

    done editing and deleting of quizzes

--- src/quiz/quiz.routes.ts
+++ src/quiz/quiz.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { getAllQuizzes, createQuiz } from './quiz.controller';
+import { getAllQuizzes, createQuiz,getQuizById, deleteQuizById, updateQuiz } from './quiz.controller';
 import { validation } from '../middleware/validate.middleware';
 import { quizValidator } from './quiz.validator';
 
 const router = express.Router();
 
 router.get('/quizzes', getAllQuizzes);
+router.get("/quizzes/:id", getQuizById);
 router.post('/create-quiz', createQuiz);
+router.delete('/quizzes/:id', deleteQuizById);
+router.put('/quizzes/:id', updateQuiz);
 
 export default router;
